b5e46349012c2266a121773c7a59f329f0c6dfc7
SlicedPotatoes <53843746+SlicedPotatoes@users.noreply.github.com>
2023-04-05 23:38:01 +0200

    load data with js

--- script.js
+++ script.js
@@ -1,7 +1,23 @@
 const filePath = "./data.json";
+const card = document.getElementById("detail-card");
 
 fetch(filePath)
   .then((response) => response.text())
   .then((fileData) => {
-    console.log(fileData);
+    for (const obj of fileData) {
+      const div = document.createElement("div");
+      const spanIcon = document.createElement("span");
+      const icon = document.createElement("img");
+      const spanText = document.createElement("span");
+      const p = document.createElement("p");
+      div.classList.add(["item", obj.category]);
+      spanText.textContent = obj.category;
+      p.textContent = obj.score + " / 100";
+      icon.src = obj.icon;
+      icon.alt = obj.category;
+      spanIcon.appendChild(icon);
+      div.appendChild(spanIcon);
+      div.appendChild(spanText);
+      div.appendChild(p);
+    }
   });
